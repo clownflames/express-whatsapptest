4d147f26005ad860d0e995f2d82d18a646dce5bd
XRS5548 <myonlineworking5548@gmail.com>
2026-05-10 12:12:57 +0530

    test

---
+++ index.js
@@ -0,0 +1,41 @@
+import { createBot } from 'whatsapp-cloud-api';
+// or if using require:
+// const { createBot } = require('whatsapp-cloud-api');
+
+(async () => {
+  try {
+    // replace the values below
+    const from = '1155502984306846';
+    const token = 'EAAY3ylkvza8BRdC1Q9fbNafgLaC3u1Kny1uXm9wqixuJL7BwORaLEPWHlperI5Y1XkjDfhm8nQlx0AqVtKNHKoZCztxVHz9tPfSm7f9V4gGTPmorXPug6yt1LkEgcNsQz4KtVH6YvHHHyYE5XZCg7sTLZAiOc37WuGyWgDmZBXhkpAniCXNP26hcjHfE5QZDZD';
+    const to = '6378695548';
+    const webhookVerifyToken = 'rohitisbest';
+
+    // Create a bot that can send messages
+    const bot = createBot(from, token);
+
+    // Send text message
+    const result = await bot.sendText(to, 'Hello world');
+
+    // Start express server to listen for incoming messages
+    // NOTE: See below under `Documentation/Tutorial` to learn how
+    // you can verify the webhook URL and make the server publicly available
+    await bot.startExpressServer({
+      webhookVerifyToken,
+      webhookPath:"/webhook",
+    });
+
+    // Listen to ALL incoming messages
+    // NOTE: remember to always run: await bot.startExpressServer() first
+    bot.on('message', async (msg) => {
+      console.log(msg);
+
+      if (msg.type === 'text') {
+        await bot.sendText(msg.from, 'Received your text message!');
+      } else if (msg.type === 'image') {
+        await bot.sendText(msg.from, 'Received your image!');
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})();
